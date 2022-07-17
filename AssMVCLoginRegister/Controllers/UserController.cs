9581eb75a3b0eac4cb06d3098d563f029f947444
Shivkarmaholic <kharadkarsandeep1@gmail.com>
2022-07-17 16:17:59 +0530

    Add project files.

---
+++ AssMVCLoginRegister/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssMVCLoginRegister.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(IFormCollection form)
+        {
+            ViewBag.Name=form["txtname"];
+            ViewBag.Password = form["txtpassword"];
+            ViewBag.Email=form["txtemail"];
+            ViewBag.Dob = form["txtdob"];
+
+            return View("Login");
+        }
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ AssMVCLoginRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AssMVCLoginRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
